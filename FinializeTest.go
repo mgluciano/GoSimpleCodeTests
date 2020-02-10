b0fe06695e5e30e9368435b2969e15d3843edcc0
Michael Luciano <michael@bytetechnology.co>
2020-02-09 16:46:41 -0800

    Updated Finialize

--- FinializeTest.go
+++ FinializeTest.go
@@ -31,7 +31,7 @@ import (
 )
 
 const  (
-  NumCol = 6
+  NumCol = 3
 )
 
 
@@ -40,13 +40,10 @@ func main() {
   Xfile  := xlsx.NewFile()
   sheet, _ := Xfile.AddSheet("Results")
 
-  fmt.Println("Row1->",WriteRow([NumCol]string{"","","1.0","","1.1",""},sheet))
-  fmt.Println("Row2->",WriteRow([NumCol]string{"order_id","QTY","EPC","QTY","EPC","Comments"},sheet))
+  fmt.Println("Row1->",WriteRow([]string{"","","1.0","","1.1","","","1.2",""},sheet))
+  fmt.Println("Row2->",WriteRow([]string{"order_id","QTY","EPC","QTY","EPC","Comments","QTY","EPC","Comments"},sheet))
 
 
-  tr := &http.Transport{
-      TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-  }
 
   flag.Parse()
   inputf := flag.Arg(0)
@@ -62,116 +59,17 @@ func main() {
       orderId:=scanner.Text();
       fmt.Printf("->%s<-",orderId)  // token in unicode-char
 
-      aurl0 := "https://finalize-testing.bytetech.co/finalize_test?version=1.0&order_id="+orderId
-      fmt.Println(aurl0)  // token in unicode-char
-      //jar, _ := cookiejar.New(nil)
-      client := &http.Client{Transport: tr}
+      result:=ProcessRequest("1.0",orderId)
+      result2:=ProcessRequest("1.1",orderId)
+      result3:=ProcessRequest("1.2",orderId)
+      r1, r2 := compare(orderId,result,result2)
+      _, r3 := compare(orderId,result,result3)
 
-      a0req, err := http.NewRequest("GET", aurl0, nil)
-
-      if err != nil {
-        fmt.Println("ERROR",err)  // token in unicode-char
-      }
-
-      a0req.Header.Add("Authorization", "KALSDFIO9RWR02JFOWEFJ20JIWEFOI")
-
-
-      a0res,err1 := client.Do(a0req)
-//      fmt.Println("BERROR2",err1)  // token in unicode-char
-      if err != nil {
-        fmt.Println("ERROR2",err1)  // token in unicode-char
-      }
-
-      defer a0res.Body.Close()
-
-  		body, err2 :=  ioutil.ReadAll(a0res.Body)
-      if err2 != nil {
-        fmt.Println("ERROR3",err2)  // token in unicode-char
-      }
-
-
-	    //fmt.Printf("\nRAW0\n%s\n",string(body))
-      var result map[string]map[string][]map[string]interface{}
-      json.Unmarshal([]byte(body),&result)
-//      fmt.Printf("\n\n\nJSON0\n%v\n\n\n",result)
-//      fmt.Printf("\n\n\nJSONA\n%v\n%d\n\n",result["order_info"]["initial_items"],len(result["order_info"]["initial_items"]))
-//      fmt.Printf("\n\n\nJSONB\n%v\n\n\n",result["run_results"]["post_run_items"])
-
-
-
-
-//      fmt.Println("LN",result)
-
-      aurl1 := "https://finalize-testing.bytetech.co/finalize_test?version=1.1&order_id="+orderId
-      fmt.Println(aurl1)  // token in unicode-char
-
-      a1req, err01 := http.NewRequest("GET", aurl1, nil)
-
-      if err01 != nil {
-        fmt.Println("ERROR",err01)  // token in unicode-char
-      }
-
-      a1req.Header.Add("Authorization", "KALSDFIO9RWR02JFOWEFJ20JIWEFOI")
-
-
-      a1res,err11 := client.Do(a1req)
-//      fmt.Println("BERROR2",err1)  // token in unicode-char
-      if err11 != nil {
-        fmt.Println("ERROR2",err11)  // token in unicode-char
-      }
-
-      defer a1res.Body.Close()
-
-      body, err12 :=  ioutil.ReadAll(a1res.Body)
-      if err12 != nil {
-        fmt.Println("ERROR3",err12)  // token in unicode-char
-      }
-      var result2 map[string]map[string][]map[string]string
-      json.Unmarshal([]byte(body),&result2)
-
-      if(len(result["run_results"]["post_run_items"])!=len(result2["run_results"]["post_run_items"])){
-        fmt.Printf("\nLength is Different\nv1.0 %d\n1.1 %d\n",len(result["run_results"]["post_run_items"]),len(result2["run_results"]["post_run_items"]))
-        v1string :="";
-        v2string :="";
-        for i:=0; i<len(result["run_results"]["post_run_items"]);i++ {
-          fmt.Printf("%d\t%s\n",i,result["run_results"]["post_run_items"][i]["epc"])
-          v1string=fmt.Sprintf("%s\n%s\n",result["run_results"]["post_run_items"][i]["epc"],v1string)
-        }
-        fmt.Println("\nv1.1\n")
-        for i:=0; i<len(result2["run_results"]["post_run_items"]);i++ {
-          fmt.Printf("%d\t%s",i,result2["run_results"]["post_run_items"][i]["epc"])
-          v2string=fmt.Sprintf("%s\n%s",result2["run_results"]["post_run_items"][i]["epc"],v2string)
-        }
-        _ = WriteRow([NumCol]string{orderId,string(len(result["run_results"]["post_run_items"])),v1string,string(len(result2["run_results"]["post_run_items"])),v2string,"Length is Different"},sheet)
-
-      }else{
-        v1string :="";
-        v2string :="";
-        no_match := false;
-        for j:=0; j<len(result["run_results"]["post_run_items"]);j++ {
-
-            v1string=fmt.Sprintf("%s\n%s",result["run_results"]["post_run_items"][j]["epc"],v1string)
-            v2string=fmt.Sprintf("%s\n%s",result2["run_results"]["post_run_items"][j]["epc"],v2string)
-            if( result["run_results"]["post_run_items"][j]["epc"] != result2["run_results"]["post_run_items"][j]["epc"]) {
-              fmt.Printf("\nValues Don't Match\n%s\n%s\n",result["run_results"]["post_run_items"][j],result["run_results"]["post_run_items"][j])
-              no_match=true
-            }
-        }
-        if (no_match){
-          WriteRow([NumCol]string{orderId,string(len(result["run_results"]["post_run_items"])),v1string,string(len(result2["run_results"]["post_run_items"])),v2string,"Values Don't Match"},sheet)
-        }else{
-          WriteRow([NumCol]string{orderId,string(len(result["run_results"]["post_run_items"])),v1string,string(len(result2["run_results"]["post_run_items"])),v2string,"Match"},sheet)
-        }
-
-
-
-      }
-      // HANDLE
+//      fmt.Println(append(append(r1,r2...),r3...))
+      _ = WriteRow(append(append(r1,r2...),r3...),sheet)
 
 
     } // End of for scanner.Scan()
-//    fmt.Println("Done with file Read\nNow printing Totals for\nGrades:")
-
 
 
     err = Xfile.Save(inputf+".xlsx")
@@ -186,12 +84,12 @@ func main() {
 order_id	QTY	EPC	QTY	EPC
 */
 
-func WriteRow (text [NumCol]string,sheet *xlsx.Sheet ) (bool){
+func WriteRow (text []string,sheet *xlsx.Sheet ) (bool){
   var row *xlsx.Row
   var cell *xlsx.Cell
 
-  for i := 0; i < NumCol; i++ {
-    fmt.Println("inWrite->",text[i])
+  for i := 0; i < len(text); i++ {
+//    fmt.Println("inWrite->",text[i])
     if i == 0 {
       row = sheet.AddRow()
     }
@@ -199,4 +97,84 @@ func WriteRow (text [NumCol]string,sheet *xlsx.Sheet ) (bool){
       cell.Value = text[i]
   }
     return true
+}
+
+
+func ProcessRequest(ver string,orderId string) (map[string]map[string][]map[string]string){
+
+    tr := &http.Transport{
+        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+    }
+
+    client := &http.Client{Transport: tr}
+
+    aurl1 := "https://finalize-testing.bytetech.co/finalize_test?version="+ver+"&order_id="+orderId
+    fmt.Println(aurl1)  // token in unicode-char
+
+    a1req, err01 := http.NewRequest("GET", aurl1, nil)
+
+    if err01 != nil {
+      fmt.Println("ERROR",err01)  // token in unicode-char
+    }
+
+    a1req.Header.Add("Authorization", "KALSDFIO9RWR02JFOWEFJ20JIWEFOI")
+
+
+    a1res,err11 := client.Do(a1req)
+  //      fmt.Println("BERROR2",err1)  // token in unicode-char
+    if err11 != nil {
+      fmt.Println("ERROR2",err11)  // token in unicode-char
+    }
+
+    defer a1res.Body.Close()
+
+    body, err12 :=  ioutil.ReadAll(a1res.Body)
+    if err12 != nil {
+      fmt.Println("ERROR3",err12)  // token in unicode-char
+    }
+    var res map[string]map[string][]map[string]string
+    json.Unmarshal([]byte(body),&res)
+
+    return res
+}
+
+func compare (orderId string ,result map[string]map[string][]map[string]string, result2 map[string]map[string][]map[string]string) ([]string,[]string){
+  res_len :=len(result["run_results"]["post_run_items"])
+  res2_len :=len(result2["run_results"]["post_run_items"])
+
+  if(res_len !=res2_len){
+    fmt.Printf("\nLength is Different\nv1.0 %d\n1.1 %d\n",res_len,res2_len)
+    v1string :="";
+    v2string :="";
+    for i:=0; i<res_len;i++ {
+      fmt.Printf("%d\t%s\n",i,result["run_results"]["post_run_items"][i]["epc"])
+      v1string=fmt.Sprintf("%s\n%s\n",result["run_results"]["post_run_items"][i]["epc"],v1string)
+    }
+    fmt.Println("\nv1.1\n")
+    for i:=0; i<res2_len;i++ {
+      fmt.Printf("%d\t%s",i,result2["run_results"]["post_run_items"][i]["epc"])
+      v2string=fmt.Sprintf("%s\n%s",result2["run_results"]["post_run_items"][i]["epc"],v2string)
+    }
+    return []string{orderId,fmt.Sprintf("%d",res_len),v1string}, []string{fmt.Sprintf("%d",res2_len),v2string,"Length is Different"}
+
+  }else{
+    v1string :="";
+    v2string :="";
+    no_match := false;
+    for j:=0; j<res_len;j++ {
+
+        v1string=fmt.Sprintf("%s\n%s",result["run_results"]["post_run_items"][j]["epc"],v1string)
+        v2string=fmt.Sprintf("%s\n%s",result2["run_results"]["post_run_items"][j]["epc"],v2string)
+        if( result["run_results"]["post_run_items"][j]["epc"] != result2["run_results"]["post_run_items"][j]["epc"]) {
+          fmt.Printf("\nValues Don't Match\n%s\n%s\n",result["run_results"]["post_run_items"][j],result["run_results"]["post_run_items"][j])
+          no_match=true
+        }
+    }
+    if (no_match){
+      return []string{orderId,fmt.Sprintf("%d",res_len),v1string},[]string{fmt.Sprintf("%d",res2_len),v2string,"Values Don't Match"}
+    }else{
+      return []string{orderId,fmt.Sprintf("%d",res_len),v1string},[]string{fmt.Sprintf("%d",res2_len),v2string,"Match"}
+    }
+
+  }
 }
